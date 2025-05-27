1f99bf581bc377163ccac53d1245f9797d082d22
William Yang <williamyang@deloitte.com.au>
2025-05-27 22:56:22 +1000

    FEAT: for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   images: {
     domains: ["lovable.dev"],
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
   // If you need to transpile specific modules
   transpilePackages: [],
   devIndicators: false,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
 export default nextConfig;
